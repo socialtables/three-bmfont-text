5f79e2ab74e14254e77fc129dda635417f591119
ramshackle-jamathon <josephvandrunen@gmail.com>
2016-03-30 12:50:10 -0400

    deps

--- test/test-2d.js
+++ test/test-2d.js
@@ -8,9 +8,13 @@
   })
  */
 
-var THREE = require('three')
+global.THREE = require('three')
+var OrbitControls = require('three-orbit-controls')(THREE)
 var createOrbitViewer = require('three-orbit-viewer')(THREE)
-var createText = require('../')
+var createText = require('../')(THREE)
+var loremIpsum = require('lorem-ipsum')
+var glslify = require('glslify')
+var SDFShader = require('../shaders/sdf')
 
 require('./load')({
   font: 'fnt/Lato-Regular-64.fnt',
@@ -26,34 +30,59 @@ function start (font, texture) {
   })
 
   app.camera = new THREE.OrthographicCamera()
-  app.camera.left = 0
-  app.camera.top = 0
-  app.camera.near = -100
-  app.camera.far = 100
+  app.camera.left = -1
+  app.camera.right = 1
+  app.camera.top = 1
+  app.camera.bottom = -1
+  app.camera.near = 1
+  app.camera.far = 10000
 
+  app.camera.position.set(0, 0, 1);
+
+  controls = new OrbitControls(app.camera, app.renderer.domElement)
+  controls.enableRotate = false
+  controls.mouseButtons = { PAN: THREE.MOUSE.LEFT, ZOOM: THREE.MOUSE.MIDDLE, ORBIT: THREE.MOUSE.RIGHT } // swapping left and right buttons
+
+  output = loremIpsum({
+      count: 20                      // Number of words, sentences, or paragraphs to generate.
+    , units: 'paragraphs'            // Generate words, sentences, or paragraphs.
+    , sentenceLowerBound: 5         // Minimum words per sentence.
+    , sentenceUpperBound: 15        // Maximum words per sentence.
+    , paragraphLowerBound: 40       // Minimum sentences per paragraph.
+    , paragraphUpperBound: 50       // Maximum sentences per paragraph.
+    , format: 'plain'               // Plain text or html
+    , random: Math.random           // A PRNG function. Uses Math.random by default
+  });
   var geom = createText({
-    text: 'this bitmap text\nis rendered with \nan OrthographicCamera',
+    text: "hey",
     font: font,
     align: 'left',
-    width: 700,
-    flipY: texture.flipY
   })
+  geom.center();
 
-  var material = new THREE.MeshBasicMaterial({
+  texture.needsUpdate = true
+  texture.minFilter = THREE.LinearMipMapLinearFilter
+  texture.magFilter = THREE.LinearFilter
+  texture.generateMipmaps = true
+
+  var materialBasic = new THREE.MeshBasicMaterial({
     map: texture,
     transparent: true,
     color: 'rgb(230, 230, 230)'
   })
 
-  var layout = geom.layout
-  var text = new THREE.Mesh(geom, material)
-  var padding = 40
-  text.position.set(padding, -layout.descender + layout.height + padding, 0)
+  var materialShader = new THREE.RawShaderMaterial(SDFShader({
+    map: texture,
+    transparent: true,
+    color: 'rgb(230, 230, 230)'
+  }))
 
-  var textAnchor = new THREE.Object3D()
-  textAnchor.add(text)
-  textAnchor.scale.multiplyScalar(1 / (window.devicePixelRatio || 1))
-  app.scene.add(textAnchor)
+  var layout = geom.layout
+
+  var mesh = new THREE.Mesh(geom, materialBasic)
+  mesh.position.set(layout.width, layout.height, 0)
+  app.scene.add(mesh)
+
 
   // update orthographic
   app.on('tick', function () {
